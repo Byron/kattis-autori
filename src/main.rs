d43795e1f8f99e995f20181e1a900782c591e95f
Sebastian Thiel <byronimo@gmail.com>
2018-11-11 14:45:16 +0100

    Solution

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use std::{io, io::Read};
 fn main() -> Result<(), Box<std::error::Error + 'static>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+    let out: String = input.split('-').filter_map(|n| n.chars().next()).collect();
+    println!("{}", out);
     Ok(())
 }
